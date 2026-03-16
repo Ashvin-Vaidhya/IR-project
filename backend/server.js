48b3ce86ffe78a28e5c5ca2382f1683d8c378e43
Ashvin-Vaidhya <ashwinvaidya34@gmail.com>
2026-03-16 23:21:19 +0530

    project final done

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 });
 
 // Middleware
-app.use(cors({ origin: '*', methods: ['GET','POST','PUT','DELETE'], allowedHeaders: ['Content-Type','Authorization'] }));
+app.use(cors({ origin: 'https://ir-project-umber.vercel.app/', methods: ['GET','POST','PUT','DELETE'], allowedHeaders: ['Content-Type','Authorization'] }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
